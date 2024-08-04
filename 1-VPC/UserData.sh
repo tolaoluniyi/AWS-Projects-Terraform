d3d4def0fd629dfcdd8ec19536ebc87017b4e993
Temi <170970335+tolaoluniyi@users.noreply.github.com>
2024-08-04 07:50:01 -0400

    userdata.sh changes

--- 1-VPC/UserData.sh
+++ 1-VPC/UserData.sh
@@ -2,6 +2,7 @@
 sudo apt update -y
 sudo apt install -y httpd
 sudo service httpd start
+sudo systemctl start httpd
+sudo systemctl enable httpd
 sudo chkconfig httpd on
-echo "Hello World Welcome to Cloud Convo. This is server 1"
-/var/www/html/index.html
+echo <h1>"Hello World Welcome to Cloud Convo. This is server 1"</h1> > /var/www/html/index.html
